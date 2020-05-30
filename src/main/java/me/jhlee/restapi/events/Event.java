ec007fc3615030d244ef889ec69d580858a33afa
이준한 <jhlee83@interpark.com>
2020-05-30 21:02:18 +0900

    testClass 생성

---
+++ src/main/java/me/jhlee/restapi/events/Event.java
@@ -0,0 +1,14 @@
+package me.jhlee.restapi.events;
+
+/**
+ * JavaDoc용 클래스의 용도를 설명합니다.
+ *
+ * @author 서비스INFRA팀 TA파트 이준한
+ * @version 1.0
+ * <pre>
+ * 2020.05.30 : 최초 작성
+ * </pre>
+ * @since 2020-05-30
+ */
+public class Event {
+}
