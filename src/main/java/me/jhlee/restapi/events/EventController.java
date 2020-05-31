fb050b6d0cb47681a4e8856c85d5e1dd30c75627
이준한 <jhlee83@interpark.com>
2020-05-31 19:14:10 +0900

    등록 api 일부완료, test까지

--- src/main/java/me/jhlee/restapi/events/EventController.java
+++ src/main/java/me/jhlee/restapi/events/EventController.java
@@ -1,5 +1,17 @@
 package me.jhlee.restapi.events;
 
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 /**
  * JavaDoc용 클래스의 용도를 설명합니다.
  *
@@ -10,5 +22,14 @@ package me.jhlee.restapi.events;
  * </pre>
  * @since 2020-05-31
  */
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
+
+    @PostMapping()
+    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+        URI createUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+        return ResponseEntity.created(createUri).body(event);
+    }
 }
